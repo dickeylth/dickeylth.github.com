a2abe4389f974214bec99bc01f6616ddf4bdd734
弘树 <tiehang.lth@alibaba-inc.com>
2014-09-21 23:51:47 +0800

    update blog

---
+++ deploy.js
@@ -0,0 +1,14 @@
+require('shelljs/global');
+
+var fs = require('fs');
+
+function updateManifest() {
+	var filePath = './source/site.appcache';
+	var fileContent = fs.readFileSync(filePath, 'utf-8');
+	fileContent = fileContent.replace(/# Revision (\d)*/, '# Revision ' + Date.now());
+	fs.writeFileSync(filePath, fileContent);
+}
+
+updateManifest();
+exec('hexo deploy -g');
+exec('git add . -A;git commit -m "update blog";git push origin src');
